ad35063ba972b39410e009a0db668b815143fb99
codelotus <95312169+codelotusr@users.noreply.github.com>
2023-10-13 17:04:33 +0300

    test

--- src/test/java/CardNameGetterTest.java
+++ src/test/java/CardNameGetterTest.java
@@ -3,10 +3,10 @@
 import org.courseproject.Suit;
 import org.courseproject.Value;
 import org.courseproject.temp.CardNameGetter;
-import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CardNameGetterTest {
     private CardNameGetter underTest;
@@ -21,7 +21,7 @@ class CardNameGetterTest {
         String cardSuit = String.valueOf(Suit.DIAMOND);
 
         // check
-        Assertions.assertEquals("The Ace of Diamonds", underTest.getCardName(cardName, cardSuit));
+        assertEquals("The Ace of Diamonds", underTest.getCardName(cardName, cardSuit));
 
     }
 }
