904aa0bf589a40611f960eeaa9f169d8a7c80381
codelotus <95312169+codelotusr@users.noreply.github.com>
2023-10-13 18:01:59 +0300

    cleanup after successful CLI setup

--- src/main/java/org/courseproject/temp/CardNameGetter.java
+++
@@ -1,7 +0,0 @@
-package org.courseproject.temp;
-
-public class CardNameGetter {
-    public String getCardName(String name, String suit) {
-        return "The " + name + " of " + suit + ".";
-    }
-}
