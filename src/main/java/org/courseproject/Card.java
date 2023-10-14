3416c402138e83bb549070e4610f1b0b211da51d
codelotus <95312169+codelotusr@users.noreply.github.com>
2023-10-14 14:34:14 +0300

    names more fit for the game / card class

--- src/main/java/org/courseproject/Card.java
+++ src/main/java/org/courseproject/Card.java
@@ -3,12 +3,29 @@ package org.courseproject;
 import java.io.Serializable;
 
 public class Card implements Serializable {
-    private Suit suit;
+    private final Suit suit;
+    private final Rank rank;
 
-    public Card(Suit suit, int value) {
+    public Card(Suit suit, Rank rank) {
         this.suit = suit;
-        this.value = value;
+        this.rank = rank;
     }
 
-    private int value;
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public int getRankValue() {
+        return rank.value;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
 }
