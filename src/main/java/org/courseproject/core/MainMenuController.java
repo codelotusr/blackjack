4ced22bdaa532c425fdb807bdced04e2f7dd49ff
codelotus <95312169+codelotusr@users.noreply.github.com>
2023-10-17 19:28:59 +0300

    MainMenuController implemented / SettingsController

---
+++ src/main/java/org/courseproject/core/MainMenuController.java
@@ -0,0 +1,45 @@
+package org.courseproject.core;
+
+import java.util.Scanner;
+
+public class MainMenuController {
+
+    private final MainMenu mainMenu;
+    private final Scanner scanner;
+
+    public MainMenuController() {
+        this.mainMenu = new MainMenu();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void display() {
+        System.out.println(mainMenu);
+    }
+
+    public void handleUserInput() {
+        display();
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Starting the game...");
+                break;
+            case 2:
+                System.out.println("Checking bank balance...");
+                break;
+            case 3:
+                System.out.println("Cashing out...");
+                break;
+            case 4:
+                System.out.println("Opening settings...");
+                break;
+            case 5:
+                System.out.println("Thank you for playing!");
+                System.exit(1);
+            default:
+                System.out.println("Invalid choice. Please select a valid option.");
+                handleUserInput();
+                break;
+        }
+    }
+}
--- src/test/java/org/courseproject/core/MainMenuTest.java
+++ src/test/java/org/courseproject/core/MainMenuTest.java
@@ -14,6 +14,7 @@ class MainMenuTest {
                 2) Check Bank
                 3) Cash Out
                 4) Settings
+                5) Quit
                 """;
 
         assertEquals(expectedMainMenu, underTest.toString());
